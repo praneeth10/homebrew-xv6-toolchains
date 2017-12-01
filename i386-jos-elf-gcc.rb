703fd07a70fe8b318269ae7fda928747f8ed9e79
Shindo <coverxit@users.noreply.github.com>
2017-12-01 15:46:55 -0800

    Update i386-jos-elf-gcc.rb

--- i386-jos-elf-gcc.rb
+++ i386-jos-elf-gcc.rb
@@ -2,9 +2,12 @@ require 'formula'
 
 class I386JosElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
-  sha256 'b037fe5132b71ecad2ea7141ec92292b5d32427bf90fd90cde432b1d5abacc2c'
+  #url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
+  #mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
+  #sha256 'b037fe5132b71ecad2ea7141ec92292b5d32427bf90fd90cde432b1d5abacc2c'
+  url "https://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2"
+  sha256 "6c11d292cd01b294f9f84c9a59c230d80e9e4a47e5c6355f046bb36d4f358092"
   
   depends_on 'gcc@4.9'
   depends_on 'gmp@4'
