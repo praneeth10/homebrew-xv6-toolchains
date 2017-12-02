0dd6dbd4a506ad9ba1c67c37122f2db51c201828
Shindo <cn.shindo@gmail.com>
2017-12-01 19:47:25 -0800

    gcc@4.9

--- i386-elf-gcc.rb
+++ i386-elf-gcc.rb
@@ -2,53 +2,38 @@ require 'formula'
 
 class I386ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'https://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2'
-  mirror 'https://ftpmirror.gnu.org/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2'
-  sha256 '6c11d292cd01b294f9f84c9a59c230d80e9e4a47e5c6355f046bb36d4f358092'
+  url "https://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-4.9.4/gcc-4.9.4.tar.bz2"
+  sha256 "6c11d292cd01b294f9f84c9a59c230d80e9e4a47e5c6355f046bb36d4f358092"
   
   depends_on 'gmp@4'
   depends_on 'libmpc@0.8'
   depends_on 'mpfr@2'
-  depends_on 'gcc@4.9'
   depends_on 'i386-elf-binutils'
 
   def install
+    binutils = Formula.factory 'i386-elf-binutils'
+
     ENV['CC'] = '/usr/local/bin/gcc-4.9'
     ENV['CXX'] = '/usr/local/bin/g++-4.9'
     ENV['CPP'] = '/usr/local/bin/cpp-4.9'
     ENV['LD'] = '/usr/local/bin/gcc-4.9'
-
-    binutils = Formulary.factory 'i386-elf-binutils'
-    ENV['PATH'] += ':#{binutils.prefix/"bin"}'
-
-    args = [
-      
-    ]
+    ENV['PATH'] += ":#{binutils.prefix/"bin"}"
 
     mkdir 'build' do
-      unless MacOS::CLT.installed?
-        # For Xcode-only systems, we need to tell the sysroot path.
-        # "native-system-headers" will be appended
-        args << "--with-native-system-header-dir=/usr/include"
-        args << "--with-sysroot=#{MacOS.sdk_path}"
-      end
-      
-      system '../configure', '--disable-nls', 
-                             '--target=i386-elf', 
-                             '--disable-werror',
+      system '../configure', '--disable-nls', '--target=i386-elf', '--disable-werror',
                              "--prefix=#{prefix}",
-                             '--enable-languages=c',
-                             '--without-headers',
+                             "--enable-languages=c",
+                             "--without-headers",
                              "--with-gmp=#{Formula["gmp@4"].opt_prefix}",
                              "--with-mpfr=#{Formula["mpfr@2"].opt_prefix}",
                              "--with-mpc=#{Formula["libmpc@0.8"].opt_prefix}"
-
       system 'make all-gcc'
       system 'make install-gcc'
-      FileUtils.ln_sf binutils.prefix/'i386-elf', prefix/'i386-elf"'
+      FileUtils.ln_sf binutils.prefix/"i386-elf", prefix/"i386-elf"
       system 'make all-target-libgcc'
       system 'make install-target-libgcc'
-      FileUtils.rm_rf share/'man'/'man7'
+      FileUtils.rm_rf share/"man"/"man7"
     end
   end
 end
