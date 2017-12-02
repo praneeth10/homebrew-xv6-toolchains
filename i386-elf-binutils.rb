5b7e5e265676025ae5a65ae2f396cd5c61603fe0
Shindo <cn.shindo@gmail.com>
2017-12-01 19:01:39 -0800

    remove depend

--- i386-elf-binutils.rb
+++ i386-elf-binutils.rb
@@ -4,9 +4,7 @@ class I386ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
   sha256 '7909a08eabdbaac0f7a22e9ede82a66ba70acd50629b045e705af864eef10b65'
-
-  depends_on 'gcc@4.9' => :build
-
+  
   def install
     mkdir 'build' do
       system '../configure', '--disable-nls', '--target=i386-elf',
