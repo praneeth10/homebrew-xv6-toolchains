0b94c4cf33639351188aba54947174fb91e6e840
Praneeth Kacham <praneethkacham@gmail.com>
2018-02-12 15:41:23 +0530

    Update i386-elf-binutils.rb

--- i386-elf-binutils.rb
+++ i386-elf-binutils.rb
@@ -2,8 +2,7 @@ require 'formula'
 
 class I386ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
-  sha256 '7909a08eabdbaac0f7a22e9ede82a66ba70acd50629b045e705af864eef10b65'
+  url 'http://ftpmirror.gnu.org/binutils/binutils-2.21.1.tar.bz2'
 
   depends_on 'gcc@4.9' => :build
 
