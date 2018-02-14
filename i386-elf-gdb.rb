f17d0f059bb02bd2e38c3f61e7bea6f67aeee7ea
Praneeth Kacham <praneethkacham@gmail.com>
2018-02-14 15:51:10 +0530

    Update i386-elf-gdb.rb

--- i386-elf-gdb.rb
+++ i386-elf-gdb.rb
@@ -1,7 +1,6 @@
 require 'formula'
 
 class I386ElfGdb < Formula
-  homepage 'http://www.gnu.org/software/gdb/'
   url 'http://mirrors.kernel.org/sourceware/gdb/releases/gdb-8.1.tar.gz'
 
   depends_on 'i386-elf-gcc'
